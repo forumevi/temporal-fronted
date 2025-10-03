e9dc34f7ddffc3cd127038eb77953dc4376adc7d
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-03 22:41:44 +0300

    Create DAOTombstones.jsx

---
+++ components/DAOTombstones.jsx
@@ -0,0 +1,7 @@
+export function DAOTombstones() {
+  return (
+    <div className="bg-gray-800 rounded-lg p-6 text-gray-400 italic">
+      Finalize edilmiş DAO'lar (DAO Tombstones) burada görünecek.
+    </div>
+  );
+}
