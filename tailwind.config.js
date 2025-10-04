b8a4667023dd97b5dd8b0f8ac97f316a6e8ffb64
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-04 12:38:33 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
+  "./app/**/*.{js,jsx,ts,tsx}",   // ← app klasöründeki tüm JS/JSX dosyalarını tarar
+  "./components/**/*.{js,jsx,ts,tsx}", // ← components klasöründeki tüm JS/JSX dosyalarını tarar
+],
   theme: {
     extend: {},
   },
